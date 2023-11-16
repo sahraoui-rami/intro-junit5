86c56ec2c15709412bfaa3484b555d17656988c9
ChefServiceInfo <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>
2023-11-16 17:09:40 +0100

    Initializing properties with @BeforeEach setUp method.

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,5 +1,6 @@
 package guru.springframework;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,15 +10,20 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class GreetingTest {
 
+    Greeting greeting;
+    @BeforeEach
+    void setUp() {
+        System.out.println("In Before Each...");
+        greeting = new Greeting();
+    }
+
     @Test
     void helloWorld() {
-        Greeting greeting = new Greeting();
         System.out.println(greeting.helloWorld());
     }
 
     @Test
     void testHelloWorld() {
-        Greeting greeting = new Greeting();
         System.out.println(greeting.helloWorld("Rami"));
     }
 }
