b0e50536a88136d25e1158173ba5bc51f5d092b2
ChefServiceInfo <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>
2023-11-16 17:32:11 +0100

    Demonstrating test lifecycle: @BeforeAll beforeClass method. @BeforeEach setUp method. @AfterEach tearDown method. @AfterAll afterClass method.

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,7 +1,6 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,19 +10,39 @@ import static org.junit.jupiter.api.Assertions.*;
 class GreetingTest {
 
     Greeting greeting;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("########################################");
+        System.out.println("Before - I am only called once!!!\n");
+    }
+
     @BeforeEach
     void setUp() {
-        System.out.println("In Before Each...");
+        System.out.println("\t*********************");
+        System.out.println("\tIn Before Each...");
         greeting = new Greeting();
     }
 
     @Test
     void helloWorld() {
-        System.out.println(greeting.helloWorld());
+        System.out.println("\t\t" + greeting.helloWorld());
     }
 
     @Test
     void testHelloWorld() {
-        System.out.println(greeting.helloWorld("Rami"));
+        System.out.println("\t\t" + greeting.helloWorld("Rami"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("\tIn After Each...");
+        System.out.println("\t*********************\n");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After - I am only called once!!!");
+        System.out.println("########################################\n\n");
     }
 }
